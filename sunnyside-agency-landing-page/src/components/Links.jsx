e45f9fab97f605ba11f1e85fa13d01193e672b72
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2024-03-26 02:17:35 +0200

    making Links component that can be used for nav and footer

---
+++ sunnyside-agency-landing-page/src/components/Links.jsx
@@ -0,0 +1,11 @@
+const Links = () => {
+  return (
+    <ul className="nav-ul-links">
+      <li>About</li>
+      <li>Services</li>
+      <li>Projects</li>
+    </ul>
+  )
+}
+
+export default Links

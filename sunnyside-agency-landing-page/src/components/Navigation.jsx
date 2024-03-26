8418e8a235c2ec7bea373bf589eac3f6631c62f8
Petteri Havia <petsku15@hotmail.fi>
2024-03-26 02:17:35 +0200

    making Links component that can be used for nav and footer

--- sunnyside-agency-landing-page/src/components/Navigation.jsx
+++ sunnyside-agency-landing-page/src/components/Navigation.jsx
@@ -1,16 +1,15 @@
 import pageLogo from '../assets/images/desktop/logo.svg'
+import Links from './Links'
 
 const Navigation = () => {
   return (
     <nav>
       <div className="navigation-container">
         <img src={pageLogo} />
-        <ul className="nav-ul-links">
-          <li>About</li>
-          <li>Services</li>
-          <li>Projects</li>
-          <li><button>CONTACT</button></li>
-        </ul>
+        <div className="link-container">
+          <Links />
+          <button>CONTACT</button>
+        </div>
       </div>
     </nav>
   )

9b5d936dc34d09639ad5f1a4b2dd7baf6d636c4c
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2024-03-29 06:34:48 +0200

    setting up menu hamburger icon

--- loopstudio-landing-page/src/components/Navigation.jsx
+++ loopstudio-landing-page/src/components/Navigation.jsx
@@ -1,8 +1,13 @@
 import logo from '../assets/images/logo.svg'
+import close from '../assets/images/icon-close.svg'
+import open from '../assets/images/icon-hamburger.svg'
+import { useState } from 'react'
 
-const Navigation = () => {
+const Navigation = ({ menu }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav>
+    <nav className="nav-reusable">
       <img src={logo} alt="loopstudio logo" />
       <ul>
         <li><a href="#">About</a></li>
@@ -11,6 +16,7 @@ const Navigation = () => {
         <li><a href="#">Products</a></li>
         <li><a href="#">Support</a></li>
       </ul>
+      {menu !== "main-menu" ? null : (<img src={isOpen ? close : open} className="menu-icon" alt="menu" onClick={() => setIsOpen(!isOpen)} />)}
     </nav>
   )
 }

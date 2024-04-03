e9fc0f962a389a862bcf1cb7fafaf8b5e827759f
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2024-04-03 09:48:41 +0300

    testing react context

---
+++ crowdfunding-product-page/src/context/app-context.jsx
@@ -0,0 +1,52 @@
+import React, { createContext } from "react";
+import { useState } from "react";
+
+export const AppContext = createContext(null)
+
+const AppContextProvider = ({ children }) => {
+  const initialProducts = [
+    {
+      name: "Bamboo Stand",
+      description: "You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you’ll be added to a special Backer member list.",
+      amount: 25,
+      productsLeft: 100,
+    },
+    {
+      name: "Black Edition Stand",
+      description: "You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer member list. Shipping is included.",
+      amount: 75,
+      productsLeft: 64,
+    },
+    {
+      name: "Mahogany Special Edition",
+      description: "You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added to our Backer member list. Shipping is included.",
+      amount: 200,
+      productsLeft: 0,
+    },
+  ]
+  const [bookmark, setBookmark] = useState(false)
+  const [modal, setModal] = useState(false)
+  const [products, setProducts] = useState(initialProducts)
+  const [check, setCheck] = useState(false)
+  const [pledgeAmount, setPledgeAmount] = useState(0)
+
+  return (
+    <AppContext.Provider
+      value={{
+        bookmark,
+        setBookmark,
+        modal,
+        setModal,
+        products,
+        setProducts,
+        check,
+        setCheck,
+        pledgeAmount,
+        setPledgeAmount
+      }}>
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+export default AppContextProvider
